58b0dafe6926ddcc7292dee2975bc45cbcc47f34
harshit kumar <117387068+5Harshit@users.noreply.github.com>
2022-12-27 11:53:42 +0000

    Commit

--- src/Component/Layouts/Navbar.jsx
+++ src/Component/Layouts/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar(props) {
           </ul>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
-            <lable className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.txt}</lable>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.txt}</label>
           </div>
           {/* <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
